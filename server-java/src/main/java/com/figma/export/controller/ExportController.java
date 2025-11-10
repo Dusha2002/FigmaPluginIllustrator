b8f968732390801bddef5b3b5151833389b6c6ee
Андрей <nespyatin@list.ru>
2025-11-10 19:07:19 +0700

    5 - Сделал базовый экспорт ПДФ, тепер реализую версии ПДФ ФИКС3

--- server-java/src/main/java/com/figma/export/controller/ExportController.java
+++ server-java/src/main/java/com/figma/export/controller/ExportController.java
@@ -9,7 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -19,7 +18,6 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 @RestController
-@RequestMapping("/convert")
 public class ExportController {
 
     private static final Logger logger = LoggerFactory.getLogger(ExportController.class);
